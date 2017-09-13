1736fcd4ac32795e791b703287de80354a780a86
Kapil Singh <singh.kapy@gmail.com>
2017-09-13 21:06:50 +0200

    Make Button Component reusable

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Text, TouchableOpacity} from 'react-native';
 
-const Button = ({onPress}) => {
+const Button = ({onPress, children}) => {
 
     const {buttonStyle, textStyle} = styles;
 
     return (
         <TouchableOpacity onPress={onPress} style={buttonStyle}>
             <Text style={textStyle}>
-                Click me!!!
+                {children}
             </Text>
         </TouchableOpacity>
     );
