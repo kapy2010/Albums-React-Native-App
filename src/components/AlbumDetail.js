0f6d5ca5b0d1437c8750549de3d21ee0f4af7030
Kapil Singh <singh.kapy@gmail.com>
2017-09-13 20:32:42 +0200

    Add button to card

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Text, View, Image} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ( {album }) => {
     const { title, artist, thumbnail_image , image} = album;
@@ -26,11 +27,16 @@ const AlbumDetail = ( {album }) => {
                     <Text>{artist}</Text>
                 </View>
             </CardSection>
+
             <CardSection>
                 <Image
                     style={imageStyle}
                     source={{uri: image}}
                 />
+            </CardSection>
+
+            <CardSection>
+                <Button/>
             </CardSection>
         </Card>
     );
