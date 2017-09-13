908f6c81f310a4ddb3a3928a91f8712ccefc1033
Kapil Singh <singh.kapy@gmail.com>
2017-09-13 20:14:39 +0200

    Make view scrollable

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import AlbumDetail from "./AlbumDetail";
 
 class AlbumList extends Component {
@@ -24,9 +24,9 @@ class AlbumList extends Component {
         console.log(this.state.albums);
 
         return (
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>
+            </ScrollView>
         );
     }
 }
